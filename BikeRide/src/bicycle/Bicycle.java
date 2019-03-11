5d8d216b24247e22f253e59313418a686b53691a
ayolicias <ziyandandlamafa47@gmail.com>
2019-03-11 16:19:42 +0200

    first commit

---
+++ BikeRide/src/bicycle/Bicycle.java
@@ -0,0 +1,21 @@
+package bicycle;
+
+public class Bicycle {
+    private int speed;
+//    private int currentSpeed;
+
+    public void accelerate(){
+        speed +=5;
+    }
+
+    public void brake(){
+        speed -=3;
+    }
+
+    public int currentSpeed(){
+        return speed = speed + 5;
+    }
+    public void stop(){
+        speed = 0;
+    }
+}
---
+++ BikeRide/tests/bicycles/BicycleTest.java
@@ -0,0 +1,28 @@
+package bicycles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BicycleTest {
+    @Test
+    public void shouldAccellerate() {
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void shouldBrake(){
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void currentSpeed(){
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void stop(){
+        assertEquals(2,2);
+
+    }
+}
